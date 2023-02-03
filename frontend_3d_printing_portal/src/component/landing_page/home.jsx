3e290f942069c3694dbb5168a33d237c88ca2a50
So'6 Rallye <73848132+So6Rallye@users.noreply.github.com>
2023-02-03 21:30:16 +0100

    Update home.jsx

--- frontend_3d_printing_portal/src/component/landing_page/home.jsx
+++ frontend_3d_printing_portal/src/component/landing_page/home.jsx
@@ -4,12 +4,12 @@ export const HomePage = () => (
         <ul className={style.companies}>
             <h2 className={style.forcompanies}>For Companies</h2>
             <p className={style.para}>We are providing best market place to build your 3d model faster.</p>
-            <a className={style.cmploginbtn} href='/company'>Login As Guest</a>
+            <link className={style.cmploginbtn} href='/company'>Login As Guest</link>
         </ul>
         <ul className={style.designers}>
             <h2 className={style.forcompanies}>For Designers</h2>
             <p className={style.para}>Join us take a look at projects and start working. Its time to groww.</p>
-            <a className={style.designerlgnbtn} href='/designers'>Login As Guest</a>
+            <link className={style.designerlgnbtn} href='/designers'>Login As Guest</link>
         </ul>
     </div>
   );
